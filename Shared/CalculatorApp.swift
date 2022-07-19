f438527576a94ab031acd5b3c226bcd0ba330674
Wallace Silva <ssouza.wallace@gmail.com>
2022-07-19 18:04:09 -0300

    Initial Commit

---
+++ Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Wallace Silva on 19/07/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
